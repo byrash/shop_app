733f7e2f64c037638d1f8a39034f146d5d3554c8
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2021-08-16 19:25:21 +1000

    Consumer Widget

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -10,42 +10,49 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var product = Provider.of<Product>(context);
+    // var product = Provider.of<Product>(context);
 
-    return GridTile(
-      child: GestureDetector(
-        onTap: () {
-          Navigator.of(context)
-              .pushNamed(ProductDetail.routeContextPath, arguments: product.id);
-        },
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
-        ),
-      ),
-      footer: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_outline),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-            color: Theme.of(context).colorScheme.secondary,
+    return Consumer<Product>(
+      builder: (context, product,  child) => ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: GridTile(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed(ProductDetail.routeContextPath,
+                    arguments: product.id);
+              },
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            footer: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: GridTileBar(
+                leading: IconButton(
+                  icon: Icon(product.isFavorite
+                      ? Icons.favorite
+                      : Icons.favorite_outline),
+                  onPressed: () {
+                    product.toggleFavoriteStatus();
+                  },
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                backgroundColor: Colors.black87,
+                title: Text(
+                  product.title,
+                  textAlign: TextAlign.center,
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {},
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+            ),
           ),
-          backgroundColor: Colors.black87,
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 }

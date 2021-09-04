f1738ccd3ec7ca6ca98a720a395a8ab3e958efc2
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2021-09-05 08:08:16 +1000

    Future builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,33 +4,33 @@ import 'package:shop_app/providers/orders.dart' show Orders;
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeContextPath = "/orders";
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Orders"),
       ),
-      body: ListView.builder(
-        itemCount: ordersData.orders.length,
-        itemBuilder: (context, index) {
-          return OrderItem(order: ordersData.orders[index]);
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null) {
+            return const Text("An error occured !!");
+          } else {
+            return Consumer<Orders>(builder: (context, ordersData, child) {
+              return ListView.builder(
+                itemCount: ordersData.orders.length,
+                itemBuilder: (context, index) {
+                  return OrderItem(order: ordersData.orders[index]);
+                },
+              );
+            });
+          }
         },
       ),
       drawer: const AppDrawer(),
